02a1206d8dd7c0b57344c5ed2a4527ec62f07a9d
KTAGit <121195747+KTAGit@users.noreply.github.com>
2026-06-16 21:45:46 -0400

    Add Footer component and styling

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Navbar } from './Navbar'
 import { Hero } from './Hero'
 import { Categories } from './Categories'
 import { SignUp } from './SignUp'
+import { Footer } from './Footer'
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
     <Hero />
     <Categories />
     <SignUp />
+    <Footer />
     </>
   )
 }
---
+++ src/Footer.jsx
@@ -0,0 +1,8 @@
+export function Footer() {
+    return (
+        <div className="footer-container">
+            <h1>VALERIUS</h1>
+            <p>© 2026 VALERIUS Digital Systems. All rights reserved.</p>
+        </div>
+    )
+}
