95914a791140685962c981b6ce956195ab7ab4f0
KTAGit <121195747+KTAGit@users.noreply.github.com>
2026-06-19 23:25:51 -0400

    Implement hamburger menu for mobile navigation

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,22 +1,26 @@
 import cartIcon from "./assets/icons/shopping-cart-icon.png"
+import menuIcon from "./assets/icons/menu.png"
+import { useState } from "react"
+
 export function Navbar() {
+    const [isActive, setIsActive] = useState(false)
+
     return (
         <div className="navbar-container">
             <div className="navbar-content-wrapper">
                 <div className="title-wrapper">
-             <h1 className="title">VALERIUS</h1>
-           </div>
-            <div className="category-wrapper">
-                <p>Home</p>
-                <p>Shop</p>
-                <p>Categories</p>
+                    <img className="menu-icon" src={menuIcon} alt="" onClick={() => setIsActive(!isActive)}/>
+                    <h1 className="title">VALERIUS</h1>
+                </div>
+                <div className={isActive ? "category-wrapper active" : "category-wrapper"}>
+                    <p>Home</p>
+                    <p>Shop</p>
+                    <p>Categories</p>
+                </div>
+                <div className="cart-wrapper">
+                    <img src={cartIcon} alt="" />
+                </div>
             </div>
-            <div className="cart-wrapper">
-                <img src={cartIcon} alt="" />
-            </div>
-            </div>
-           
-
         </div>
     )
 }
