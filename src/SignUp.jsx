7e5b6fbf75f156e0056a3e754a36882d1df10d70
KTAGit <121195747+KTAGit@users.noreply.github.com>
2026-06-22 20:38:25 -0400

    Add simple email validation for signup

--- src/SignUp.jsx
+++ src/SignUp.jsx
@@ -1,11 +1,22 @@
+import { useState } from "react"
+
 export function SignUp () {
+    const [email, setIsEmail] = useState("")
+    function handleSubmit(e) {
+        e.preventDefault()
+        if (!e.target.form.checkValidity()) return
+        setIsEmail("Thank You!")
+        setTimeout(() => {
+            setIsEmail("")
+        }, 3000)
+    }
     return (
         <div className="sign-up-container">
             <h1>Join the VALERIUS Collective</h1>
             <p>Sign up to receive early access to new drops and exclusive member-only <br /> collections.</p>
             <form action="#">
-                <input type="email" name="email" required placeholder="Your email address"/>
-                <button className="subscribe-btn">Subscribe</button>
+                <input type="email" name="email" value={email} required placeholder="Your email address" onChange={(e) => setIsEmail(e.target.value) }/>
+                <button type="submit" className="subscribe-btn" onClick={handleSubmit}>Subscribe</button>
             </form>
         </div>
     )
