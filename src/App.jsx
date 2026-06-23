8f97d153257ae4cf456753348f3fc982c3a90e14
KTAGit <121195747+KTAGit@users.noreply.github.com>
2026-06-22 23:03:51 -0400

    Render fetched products in Shop using useOutletContext

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,31 @@
-import { useState } from 'react'
 import { Navbar } from './Navbar'
 import { Home } from './Home'
 import { Footer } from './Footer'
 import { Outlet } from 'react-router'
+import { useEffect, useState } from "react"
 
 function App() {
 
+const [data, setData] = useState(null)
+
+    useEffect(() =>  {
+        const loadData = async () => {
+            try {
+                const response = await fetch('https://fakestoreapi.com/products')
+                const result = await response.json()
+                setData(result)
+            }
+            catch (error) {
+                console.error("Failed to fetch:", error)
+            }
+        }
+        loadData()
+    },[])
   return (
     <>
     <Navbar />
     <div className='main-content'>
-        <Outlet />
+        <Outlet context={[data, setData]}/>
     </div>
     <Footer /> 
     </>
--- src/Shop.jsx
+++ src/Shop.jsx
@@ -1,7 +1,11 @@
 import placeholderImg from "./assets/unnamed.png"
 import star from "./assets/icons/star.png"
 import addCartIcon from "./assets/icons/add-cart.png"
+import { useOutletContext } from "react-router"
+import { useEffect } from "react"
 export function Shop() {
+    const [data, setData] = useOutletContext()
+    
     return (
         <>
         <div className="category-tags-container">
@@ -12,15 +16,16 @@ export function Shop() {
             <span className="category-tag">All Products</span>
         </div>
         <div className="cards-container">
-            <div className="card-wrapper">
-                <img src={placeholderImg} alt="" />
-                <p className="category">WEARABLE TECH</p>
-                <h2 className="item-name">Neon-Sync Headset</h2>
-                <p className="item-description">Spatial audio reimagined with biometric feedback sensors and crystalline acoustic chambers.</p>
+            {data?.map((object) => (
+            <div key={object.id} className="card-wrapper">
+                <img src={object.image} alt="" />
+                <p className="category">{object.category}</p>
+                <h2 className="item-name">{object.title}</h2>
+                <p className="item-description">{object.description}</p>
                 <div className="rating-item-quantity-wrapper">
                     <div className="item-rating">
                         <img src={star} alt="" />
-                        <p className="item-review">4.9 (124 reviews)</p>
+                        <p className="item-review">{object.rating.rate} ({object.rating.count} reviews)</p>
                     </div>
                     <div className="item-quantity-wrapper">
                         <button className="card-minus-btn">−</button>
@@ -30,10 +35,12 @@ export function Shop() {
                 </div>
                 
                 <div className="add-to-cart-wrapper">
-                    <p className="item-price">$899.00</p>
+                    <p className="item-price">${object.price}</p>
                     <span className="add-to-cart-btn"> <img src={addCartIcon} alt="" /> ADD TO CART</span>
                 </div>
             </div>
+            ))}
+            
         </div>
         
         </>
